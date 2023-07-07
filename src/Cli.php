e613f95de35eb27b22e336c4400b1d35e4e32992
Роман Соколов <sokoloff.rv@gmail.com>
2023-07-07 16:11:23 +0600

    Логика перенесена в отдельный файл

---
+++ src/Cli.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace BrainGames\Cli;
+
+use function cli\line;
+use function cli\prompt;
+
+function start()
+{
+    line("Welcome to the Brain Game!");
+    $name = prompt("May I have your name?");
+    line("Hello, $name");
+
+    return $name;
+}
+
+function playEvenGame($name)
+{
+    line("Answer 'yes' if the number is even, otherwise answer 'no'.");
+    $countCorrectAnswers = 0;
+
+    while ($countCorrectAnswers < 3 && $countCorrectAnswers >= 0) {
+        $number = rand(0, 999);
+        $answer = prompt("Question: $number");
+        $isEven = $number % 2 === 0;
+        if ($isEven && $answer === 'yes' || !$isEven && $answer === 'no') {
+            line("Correct!");
+            $countCorrectAnswers++;
+        } else {
+            $correctAnswer = $isEven ? 'yes' : 'no';
+            line("'$answer' is wrong answer ;(. Correct answer was '$correctAnswer'.\nLet's try again, $name!");
+            $countCorrectAnswers = -1;
+        }
+    }
+
+    if ($countCorrectAnswers === 3) {
+        line("Congratulations, $name!");
+    }
+}
