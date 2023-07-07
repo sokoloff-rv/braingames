45d33ad53e77f3c6249edc92bf48298df83b1239
Роман Соколов <sokoloff.rv@gmail.com>
2023-07-07 16:12:31 +0600

    Переименована функция start

--- src/Cli.php
+++ src/Cli.php
@@ -5,7 +5,7 @@ namespace BrainGames\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function start()
+function startGames()
 {
     line("Welcome to the Brain Game!");
     $name = prompt("May I have your name?");
